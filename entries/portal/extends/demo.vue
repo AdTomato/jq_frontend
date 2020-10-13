425e48cd53ff35dd28eef1aadd2c57fe2f4dbaf2
WuYujie <coffee377@dingtalk.com>
2020-10-13 10:09:52 +0800

    自定义页面

---
+++ entries/portal/extends/demo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>{{ userInfo }}</div>
+</template>
+
+<script lang="ts">
+import OAuthApi from "@/apis/oauth";
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({
+  name: 'demo',
+})
+export default class Demo extends Vue {
+
+  /* 用户信息 */
+  userInfo: any = {};
+
+  created() {
+    this.getUserInfo();
+  }
+
+  /* 获取当前用户信息 */
+  async getUserInfo() {
+    const res = await OAuthApi.getUserInfo();
+    if (res.errcode === 0) {
+      this.userInfo = res.data;
+      console.log('用户信息 => ',this.userInfo)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- entries/portal/extends/routes.ts
+++ entries/portal/extends/routes.ts
@@ -3,11 +3,17 @@ export default {
     path: '/form/detail',
     name: 'form-detail',
     component: () => import('@/views/form/form-detail.vue')
-  }
-  ,
+  },
   formCalendar: {
     path: '/form/detail/calendar',  //  /application
     name: 'form-calendar',
     component: () => import('./calendar.vue')
+  },
+  application: {
+    AA: {
+      path: '/application/ZY/demo',
+      name: 'applicationDemo',
+      component: () => import('./demo.vue'),
+    },
   }
 } as any;
